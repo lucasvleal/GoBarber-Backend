71b43da7f331da58edb2b685c64acb19065b52d9
Lucas Leal <lucasvianil@gmail.com>
2020-06-25 22:15:59 -0300

    Initial commit : connection with postgres database done

---
+++ src/routes/appointments.routes.ts
@@ -0,0 +1,36 @@
+import { Router } from 'express';
+import { parseISO } from 'date-fns';
+import { getCustomRepository } from 'typeorm';
+
+import AppointmentsRepository from '../repositories/AppointmentsRepository';
+import CreateAppointmentService from '../services/CreateAppoinmentService';
+
+const appointmentsRouter = Router();
+
+appointmentsRouter.get('/', async (request, response) => {
+    const appointmentsRepository = getCustomRepository(AppointmentsRepository);
+    const allAppointments = await appointmentsRepository.find();
+
+    return response.json(allAppointments);
+});
+
+appointmentsRouter.post('/', async (request, response) => {
+    try {
+        const { provider, date } = request.body;
+
+        const parsedDate = parseISO(date);
+
+        const createAppointment = new CreateAppointmentService();
+
+        const newAppointment = await createAppointment.execute({
+            provider,
+            date: parsedDate,
+        });
+
+        return response.json(newAppointment);
+    } catch (err) {
+        return response.status(400).json({ error: err.message });
+    }
+});
+
+export default appointmentsRouter;
