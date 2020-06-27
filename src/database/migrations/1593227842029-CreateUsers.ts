e1b58751d11e3673455881c2a96574d9ba58bfd0
Lucas Leal <lucasvianil@gmail.com>
2020-06-27 02:09:42 -0300

    Users and Sessions created : cadastrating user and appointments functional

--- src/database/migrations/1593227842029-CreateUsers.ts
+++ src/database/migrations/1593227842029-CreateUsers.ts
@@ -1,11 +1,10 @@
 import { MigrationInterface, QueryRunner, Table } from 'typeorm';
 
-export default class CreateAppointments1593130642958
-    implements MigrationInterface {
+export default class CreateUsers1593227842029 implements MigrationInterface {
     public async up(queryRunner: QueryRunner): Promise<void> {
         await queryRunner.createTable(
             new Table({
-                name: 'appointments',
+                name: 'users',
                 columns: [
                     {
                         name: 'id',
@@ -15,13 +14,14 @@ export default class CreateAppointments1593130642958
                         default: 'uuid_generate_v4()',
                     },
                     {
-                        name: 'provider',
+                        name: 'name',
                         type: 'varchar',
                         isNullable: false,
                     },
                     {
-                        name: 'date',
-                        type: 'timestamp with time zone',
+                        name: 'email',
+                        type: 'varchar',
+                        isUnique: true,
                         isNullable: false,
                     },
                     {
@@ -36,12 +36,17 @@ export default class CreateAppointments1593130642958
                         default: 'now()',
                         isNullable: false,
                     },
+                    {
+                        name: 'password',
+                        type: 'varchar',
+                        isNullable: false,
+                    },
                 ],
             }),
         );
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.dropTable('appointments');
+        await queryRunner.dropTable('users');
     }
 }
