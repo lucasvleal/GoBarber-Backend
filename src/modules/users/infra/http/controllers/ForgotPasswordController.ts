dba48f36d3501bd803cf5160eb9a65da5ba4eb0a
Lucas Leal <lucasvianil@gmail.com>
2020-07-25 01:30:51 -0300

    Reset password flow done

---
+++ src/modules/users/infra/http/controllers/ForgotPasswordController.ts
@@ -0,0 +1,30 @@
+import { Request, Response } from 'express';
+import { container } from 'tsyringe';
+
+import SendForgotPasswordEmailService from '@modules/users/services/SendForgotPasswordEmailService';
+
+export default class ForgotPasswordController {
+    public async index(
+        request: Request,
+        response: Response,
+    ): Promise<Response> {
+        return response.json({ ok: true });
+    }
+
+    public async create(
+        request: Request,
+        response: Response,
+    ): Promise<Response> {
+        const { email } = request.body;
+
+        const sendForgotPasswordEmailService = container.resolve(
+            SendForgotPasswordEmailService,
+        );
+
+        await sendForgotPasswordEmailService.execute({
+            email,
+        });
+
+        return response.status(204).json();
+    }
+}
--- src/modules/users/services/SendForgotPasswordEmailService.ts
+++ src/modules/users/services/SendForgotPasswordEmailService.ts
@@ -30,11 +30,13 @@ class SendForgotPasswordEmailService {
             throw new AppError('User does not exists.');
         }
 
-        await this.userTokenRepository.generate(userExists.id);
+        const { token } = await this.userTokenRepository.generate(
+            userExists.id,
+        );
 
-        this.mailProvider.sendMail(
+        await this.mailProvider.sendMail(
             email,
-            'Pedido de recuperação de senha recebido.',
+            `Pedido de recuperação de senha recebido: ${token}`,
         );
     }
 }
